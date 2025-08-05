a73139d07dddc1ff91a88a5ef05224f0c210e0ba
NavneetRathour <navneetkumar969605@gmail.com>
2025-08-05 17:22:31 +0000

    second

--- 01-paython/List.py
+++ 01-paython/List.py
@@ -1,2 +1,31 @@
 li = [1,2,3,"such",True,4j,345]
+print(li[1])
+print(li[0:2+1])
+print(li[-1])
+print(li[::-1])
+print(li[0:7:2])
+s = "python"
+#print(li+s)   # not work
+print(list(s))
+print(li[3][2])  
+print(li*2)
+print(len(li))
+li.append(6)   # last me add krta h
+print(li)     
+li.append(s)
+print(li)
+l = [1,2,3]
+li.append(l)
+print(li)
+li.extend("nav")  # onley eterable data add
+print(li)
+l.extend([1,2,3])
+print(l)
+l.insert(0,"navneet")
+print(l)
+l.pop(1)   # delet from first index 
+print(l)
+l.pop()   # delet from last
+print(l)
+l.remove(3)  # jo value dege usi ko uda dega
 
