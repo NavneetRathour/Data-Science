5ea5f82fe7c1f1c621dfd635d43b95bd097371b8
NavneetRathour <navneetkumar969605@gmail.com>
2025-08-05 13:21:59 +0000

    Litrelse

--- 01-paython/first.py
+++ 01-paython/first.py
@@ -2,3 +2,40 @@ a=4
 b=5
 c=a+b
 print(c)
+
+v= 8j  # complex number
+
+# indexing 
+
+name = "Navneet "
+
+print(name[1])  # forword indexing
+print(name[-3]) # backword indexing
+print(name[0:3]) # slising
+print(name[0:7:2]) # aloternet
+print(name[::2])   # sort mathod
+print(name[2::])
+print(name[::-1])  # negative direction
+print(name[2:6:-1]) # blank
+print(name[7:0])    # +1 jamp ho raha h to blanck
+print(name[7:0:-1])
+
+# string manupulation
+
+s1= "this is my first class"
+print(len(s1))
+print(s1.find("s"))  # only fist give it first
+
+# count 
+print(s1.count("s"))     # tell us how many times s is write
+print(s1.upper())
+s2 = "ASDSAFDF"
+print(s2.lower())
+print(s2.title())
+print(s1.capitalize())
+n= "navneet"
+print(n)
+n =  n+str(1)
+print(n)
+
+
