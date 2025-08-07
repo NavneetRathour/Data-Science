00e59e1ca385e54f42116f6014fbef909a00dc3b
NavneetRathour <navneetkumar969605@gmail.com>
2025-08-07 12:36:52 +0000

    loop

--- 01-paython/Else-if.py
+++ 01-paython/Else-if.py
@@ -1,10 +1,27 @@
-marks = int(input("enter your marks"))
-if marks >= 80:
-    print("you will be a part of A0 batch")
-elif marks >= 60 and marks < 80:
-    print("you will be a part of A1 batch")
-elif marks >= 40 and marks < 60:
-    print("you will be a part of A2 batch")
-else :
-    print("you will be a part of A3 batch")
+# marks = int(input("enter your marks"))
+# if marks >= 80:
+#     print("you will be a part of A0 batch")
+# elif marks >= 60 and marks < 80:
+#     print("you will be a part of A1 batch")
+# elif marks >= 40 and marks < 60:
+#     print("you will be a part of A2 batch")
+# else :
+#     print("you will be a part of A3 batch")
+
+# price = int(input("Enter price"))
+# if price > 1000:
+#     print("i will not purchase")
+# else:
+#     print("i will purchase")
    
+price = int(input("Enter price"))
+if price > 1000:
+    print("i will not purchase")
+    if price >5000:
+        print("this is to much")
+    elif price <2000:
+        print("it's ok")
+elif price <1000:
+    print("i will purchase")
+else :
+    print("not intrested")
---
+++ 01-paython/Loop.py
@@ -0,0 +1,22 @@
+# l1 = []
+# l = [1,2,3,4,4,5,6,7]
+# for i in l :
+#     print(i+1)  
+#     l1.append(i+1)
+# print(l1)
+
+# l1 = []
+# l3 = ["navneet", "zomato","momos"]
+# for i in l3 :
+    
+
+#     l1.append(i.upper())
+# print(l1) 
+   
+l = [1,2,3,4,5,"kumar","jam",234,456,"vinod"]
+l1_num = []
+l2_str = []
+for i in l:
+    if type(i) == int or type (i) == float:
+        
+
