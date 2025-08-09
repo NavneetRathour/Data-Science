78ff78ed8ab17e0e3bd8c717d7f694079ef9b753
NavneetRathour <navneetkumar969605@gmail.com>
2025-08-09 10:43:10 +0000

    while

---
+++ 01-paython/while-loop.py
@@ -0,0 +1,24 @@
+# a = 1
+
+# while a <= 10 :
+#     print(a)
+#     a = a+1
+
+# # some of the number till some point
+# n = int(input("enter your limit"))
+# star_point =0
+# count = 1
+# while count  <= n :
+#     star_point = star_point +count
+#     count = count + 1
+# print(star_point)
+
+# factorial
+num = int(input("Enter number"))
+fact = 1
+while num > 0:
+    fact = fact * num
+    num  = num -1
+print(fact)
+
+
