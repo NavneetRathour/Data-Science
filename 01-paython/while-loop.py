55daffa9c4970965db3165a7500a470c8cd2cb3d
NavneetRathour <navneetkumar969605@gmail.com>
2025-08-10 12:58:34 +0000

    comprihenction

--- 01-paython/while-loop.py
+++ 01-paython/while-loop.py
@@ -14,11 +14,75 @@
 # print(star_point)
 
 # factorial
-num = int(input("Enter number"))
-fact = 1
-while num > 0:
-    fact = fact * num
-    num  = num -1
-print(fact)
+# num = int(input("Enter number"))
+# fact = 1
+# while num > 0:
+#     fact = fact * num
+#     num  = num -1
+# print(fact)
 
+# febinaci seriese
+# number = int(input("Enter the number of fab"))
+# a ,b = 0,1
+# counter = 0
+# while counter < number :
+#     print(a)
+#     c = a + b
+#     a = b
+#     b = c
+#     counter = counter + 1
 
+# in foor
+# a,b =0,1
+# for i in range(10):
+#     print(a)
+#     c = a + b
+#     a = b
+#     b = c
+
+# word = input("enter string")
+# reverse = ""
+# length = len(word)
+
+# while length > 0 :
+#     reverse = reverse + word[length -1]
+#     length = length -1
+#     print(reverse)
+
+# table 
+
+# n = int(input("Enter number"))
+# i = 1
+# while i < 11:
+#     result = n*i
+#     print(n,"*",i ,"=" , result)
+#     i = i+1
+
+# n = 5
+# i = 1
+# while i < n :
+#     print(i)
+#     i = i+1
+# else :
+#     print("this will executed once your while will complwated is sucssesfully")
+
+# 
+# comprihention
+
+# l = [2,3,4,5,6,7,8,9]
+# l1 = []
+# for i in l:
+#     l1.append(i**2)
+# print(l1)
+
+# print([i**2 for i in l]) # one liner work
+# print([ i for i in l if i %2 ==0])   # for find even number
+
+l1 = ["navneet","data","science"]
+print([i.upper() for i in l1])
+
+ #dict comprihention
+d = {"key1" : 1 ,"ker2": 2 ,"key3":3}
+print({k:v**2 for k,v in d.items()} )
+
+print({k:v for k ,v in d.items() if v> 1})
